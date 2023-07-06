04efe6e39a8daf25ef301d6101f3dc779d64cd8f
OM1705 <ompol1705@gmail.com>
2023-07-06 18:58:55 +0530

    effef

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors())
+app.use(cors(
+  {
+    origin: ["https://i-notebook-backend-eta.vercel.app"],
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true
+  }
+))
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'));
