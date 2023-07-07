5f093ed35b037d1836a074b10d93bea8ef5950b2
OM1705 <ompol1705@gmail.com>
2023-07-07 15:09:40 +0530

    iuhi

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors(
-  {
-    origin: ["https://i-notebook-backend-eta.vercel.app"],
-    methods: ["POST", "GET", "DELETE", "PUT"],
-    credentials: true
-  }
-))
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
